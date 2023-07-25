2502e85401ecf04ab376604e5a55a55f0a5b1905
brodytiktok <kevinpratama789189@gmail.com>
2023-07-25 15:49:26 +0700

    a

--- Car Network/assets/js/main.js
+++ Car Network/assets/js/main.js
@@ -33,7 +33,36 @@ document.addEventListener('DOMContentLoaded', function () {
 
     /* Paganation using Carousel */
 
+    /* Array Devices */
+    const devices = {
+        phone : 1,
+        tablet : 2,
+        laptop : 3,
+        pc : 4,
+    }
+    function sizeLayout() {
+        const devicewidith = window.innerWidth;
+        console.log(devicewidith);
 
 
+        /* Cards */
+        const cards = document.querySelectorAll('.card');
+        let currentPage = 0;
+        const cardsPerPage = 0;
+        console.log(cards);
 
+        if (devicewidith <= 600) {
+            console.log('cards', devices.phone)
+
+        } else if (devicewidith <= 768) {
+            console.log('tablet', devices.tablet)
+        } else if (devicewidith <= 992) {
+            console.log('laptop', devices.laptop)
+        } else {
+            console.log('desktop', devices.pc)
+        }
+
+    }
+    sizeLayout();
+    window.addEventListener("resize", sizeLayout);
 });
